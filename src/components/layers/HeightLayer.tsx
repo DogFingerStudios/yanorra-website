aa588119e37797109873ce5339f706c11e5ba998
Addy <aclaure@gmail.com>
2026-06-14 14:07:49 -0400

    fixing the spacing;

--- src/components/layers/HeightLayer.tsx
+++ src/components/layers/HeightLayer.tsx
@@ -1,39 +1,39 @@
 import { GeoJSON } from 'react-leaflet'
 
 type HeightLayerEntry =
-{
-  id: string
-  data: GeoJSON.GeoJsonObject
-  options:
   {
-    pane?: string
-    color?: string
-    weight?: number
-    fillColor?: string
-    fillOpacity?: number
+    id: string
+    data: GeoJSON.GeoJsonObject
+    options:
+    {
+      pane?: string
+      color?: string
+      weight?: number
+      fillColor?: string
+      fillOpacity?: number
+    }
   }
-}
 
 type HeightColorRule =
-{
-  minHeightExclusive: number
-  color: string
-}
+  {
+    minHeightExclusive: number
+    color: string
+  }
 
 const HEIGHT_COLOR_RULES: HeightColorRule[] =
-[
-  { minHeightExclusive: 20477, color: '#ffffff' },
-  { minHeightExclusive: 17500, color: '#d9d9d9' },
-  { minHeightExclusive: 15000, color: '#b8b8b8' },
-  { minHeightExclusive: 12500, color: '#9f9f9f' },
-  { minHeightExclusive: 10000, color: '#947f74' },
-  { minHeightExclusive: 7500, color: '#9d836b' },
-  { minHeightExclusive: 5000, color: '#af9667' },
-  { minHeightExclusive: 3500, color: '#9db562' },
-  { minHeightExclusive: 2000, color: '#7eab57' },
-  { minHeightExclusive: 1000, color: '#629a46' },
-  { minHeightExclusive: 500, color: '#4a7f36' },
-]
+  [
+    { minHeightExclusive: 20477, color: '#ffffff' },
+    { minHeightExclusive: 17500, color: '#d9d9d9' },
+    { minHeightExclusive: 15000, color: '#b8b8b8' },
+    { minHeightExclusive: 12500, color: '#9f9f9f' },
+    { minHeightExclusive: 10000, color: '#947f74' },
+    { minHeightExclusive: 7500, color: '#9d836b' },
+    { minHeightExclusive: 5000, color: '#af9667' },
+    { minHeightExclusive: 3500, color: '#9db562' },
+    { minHeightExclusive: 2000, color: '#7eab57' },
+    { minHeightExclusive: 1000, color: '#629a46' },
+    { minHeightExclusive: 500, color: '#4a7f36' },
+  ]
 
 function parseFiniteNumber(value: unknown): number | null
 {
